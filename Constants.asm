03cc051102ff8535121b43159965be06bda80576
Ushupiuck <giancarloknd@hotmail.com>
2023-06-13 05:39:57 -0300

    Update Constants.asm

--- Constants.asm
+++ Constants.asm
@@ -86,52 +86,6 @@ bitL:		equ 2
 bitDn:		equ 1
 bitUp:		equ 0
 
-; Object variables (Sonic 2 disassembly nomenclature)
-render_flags:	equ 1	; bitfield for x/y flip, display mode
-art_tile:	equ 2	; palette line & VRAM setting (2 bytes)
-mappings:	equ 4	; mappings address (4 bytes)
-x_pos:		equ 8	; x-axis position (2-4 bytes)
-y_pixel:	equ $A	; y-axis position for screen-fixed items (2 bytes)
-y_pos:		equ $C	; y-axis position (2-4 bytes)
-x_vel:		equ $10	; x-axis velocity (2 bytes)
-y_vel:		equ $12	; y-axis velocity (2 bytes)
-inertia:	equ $14	; and $15 ; directionless representation of speed... not updated in the air
-y_radius:	equ $16	; height/2
-x_radius:	equ $17	; width/2
-priority:	equ $18	; sprite stack priority -- 0 is front
-width_pixels:	equ $19	; action width
-mapping_frame:	equ $1A	; current frame displayed
-anim_frame:	equ $1B	; current frame in animation script
-anim:		equ $1C	; current animation
-next_anim:	equ $1D	; next animation
-anim_frame_duration: equ $1E ; time to next frame
-collision_flags: equ $20 ; collision response type
-collision_property: equ $21 ; collision extra property
-status:		equ $22	; orientation or mode
-respawn_index:	equ $23	; respawn list index number
-routine:	equ $24	; routine number
-routine_secondary: equ $25 ; secondary routine number
-angle:		equ $26	; angle
-flip_angle:	equ $27		; angle about the x axis (360 degrees = 256) (twist/tumble)
-subtype:	equ $28	; object subtype
-flip_turned:	equ $29 ; 0 for normal, 1 to invert flipping (it's a 180 degree rotation about the axis of Sonic's spine, so he stays in the same position but looks turned around)
-obj_control:	equ $2A ; 0 for normal, 1 for hanging or for resting on a flipper, $81 for going through CNZ/OOZ/MTZ tubes or stopped in CNZ cages or stoppers or flying if Tails
-next_tilt:	equ $36 ; angle on ground in front of sprite
-tilt:		equ $37 ; angle on ground
-stick_to_convex:	equ $38 ; 0 for normal, 1 to make Sonic stick to convex surfaces like the rotating discs in Sonic 1 and 3 (unused in Sonic 2 but fully functional)
-spindash_flag:	 	equ $39 ; 0 for normal, 1 for charging a spindash or forced rolling
-pinball_mode:		= spindash_flag
-spindash_counter:	equ $3A ; and $3B
-restart_countdown	= spindash_counter; and 1+spindash_counter
-jumping: 	equ $3C
-interact:	equ $3D ; RAM address of the last object Sonic stood on, minus $FFFFB000 and divided by $40
-top_solid_bit:	equ $3E ; the bit to check for top solidity (either $C or $E)
-lrb_solid_bit:	equ $3F ; the bit to check for left/right/bottom solidity (either $D or $F)
-; Object variables used by Sonic (Sonic 2 disassembly nomenclature)
-invulnerable_time:	equ $30 ; and $31 ; time remaining until you stop blinking
-invincibility_time:	equ $32 ; and $33 ; remaining
-speedshoes_time:	equ $34 ; and $35 ; remaining
-
 ; Object variables
 obRender	= render_flags	; bitfield for x/y flip, display mode
 obGfx		= art_tile	; palette line & VRAM setting (2 bytes)
@@ -149,7 +103,7 @@ obActWid	= width_pixels	; action width
 obFrame		= mapping_frame	; current frame displayed
 obAniFrame	= anim_frame	; current frame in animation script
 obAnim		= anim		; current animation
-obNextAni	= next_anim	; next animation
+obNextAni	= prev_anim	; prev animation
 obTimeFrame	= anim_frame_duration	; time to next frame
 ;obDelayAni	equ $1F		; time to delay animation
 obColType	= collision_flags	; collision response type
@@ -173,22 +127,6 @@ afChange:	equ $FD	; run specified animation
 afRoutine:	equ $FC	; increment routine counter
 afReset:	equ $FB	; reset animation and 2nd object routine counter
 af2ndRoutine:	equ $FA	; increment 2nd routine counter
-; ---------------------------------------------------------------------------
-; unknown or inconsistently used offsets that are not applicable to sonic/tails:
-; (provided because rearrangement of the above values sometimes requires making space in here too)
-objoff_A =		2+x_pos ; note: x_pos can be 4 bytes, but sometimes the last 2 bytes of x_pos are used for other unrelated things
-objoff_B =		3+x_pos ; unused
-objoff_E =		2+y_pos	; unused
-objoff_F =		3+y_pos ; unused
-objoff_10 =		$10
-objoff_14 =		$14
-objoff_15 =		$15
-objoff_1F =		$1F
-objoff_27 =		$27
-objoff_28 =		$28 ; overlaps subtype, but a few objects use it for other things anyway
- enum               objoff_29=$29,objoff_2A=$2A,objoff_2B=$2B,objoff_2C=$2C,objoff_2D=$2D,objoff_2E=$2E,objoff_2F=$2F
- enum objoff_30=$30,objoff_31=$31,objoff_32=$32,objoff_33=$33,objoff_34=$34,objoff_35=$35,objoff_36=$36,objoff_37=$37
- enum objoff_38=$38,objoff_39=$39,objoff_3A=$3A,objoff_3B=$3B,objoff_3C=$3C,objoff_3D=$3D,objoff_3E=$3E,objoff_3F=$3F
 ; ---------------------------------------------------------------------------
 ; Background music
 ; bgm__First:	equ $81
